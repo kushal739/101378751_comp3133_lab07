7685443a93560540ba5348c1534b529e32af401b
Kushal <patelkushal846@gmail.com>
2024-03-23 20:32:38 -0400

    Lab done

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { RemoveSpacesPipe } from './remove-spaces.pipe';
+import { InputFormatDirective } from './input-format.directive'; // <-- NgModel lives here
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    RemoveSpacesPipe,
+    InputFormatDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
